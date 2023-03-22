60d63c255bb8bb2376017254efe7e548973fe2f6
Ville Haapaniemi <ville.haapaniemi@tuni.fi>
2023-03-22 17:07:49 +0200

    Country class created

---
+++ Country.cs
@@ -0,0 +1,77 @@
+﻿namespace OhSyTe
+{
+    public class Country
+    {
+        private string _code;
+        private string _name;
+        private double _area;
+        private int _population;
+        public Country(string code, string name, double area,
+        int population)
+        {
+            Code = code;
+            Name = name;
+            Area = area;
+            Population = population;
+        }
+        public string Code
+        { 
+            get => _code;     
+            set 
+            { 
+                if (value.Length != 2 || value.Any(char.IsLower))
+                {
+                    throw new ArgumentException("Country code must be 2 chars long and all uppercase!", nameof(value));
+                }
+                _code = value;
+            }
+        }
+        public string Name
+        {
+            get => _name;
+            set
+            {
+                if (value.Length == 0)
+                {
+                    throw new ArgumentException("Country name can not be empty", nameof(value));
+                }
+                _name = value;
+            }
+        }
+        public double Area
+        {
+            get => _area;
+            set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException("Country area must be positive value", nameof(value));
+                }
+                _area = value; ;
+            }
+        }
+        public int Population
+        {
+            get => _population;
+            set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException("Country population must be positive value", nameof(value));
+                }
+                _population = value;
+            }
+        }
+        public double PopulationDensity
+        {
+            get
+            {
+                return Population / Area;
+            }
+        }
+        public override string ToString()
+        {
+            return $"{Code} {Name}";
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace OhSyTe
+{
+    public class Program
+    {
+        public static void Main()
+        {   try
+            {
+                Country fin = new Country("FI", "Finland", 44, 4);
+                Console.WriteLine(fin);
+            } catch (Exception e) { Console.Error.WriteLine(e.Message); }
+        }
+    }
+}
